a27d381d974c53f70d4f5f7c2d14514805b91b8f
jaycee-cpu93 <jamescoding93@gmail.com>
2020-05-01 05:42:26 -0400

    First commit & i implemented fizzbuzz without using return keyword

---
+++ fiz_plenty.js
@@ -0,0 +1,133 @@
+
+
+
+
+function fizbuz(number){
+	
+	if ( number % 3 === 0 && number % 5 === 0 ) {
+		console.log("fizzbuzz")
+	}
+
+	if ( number % 3 === 0 ) {
+		console.log("fizz")
+	}
+
+
+	if ( number % 5 === 0 ) {
+		console.log("buzz")
+	}
+
+}
+
+fizIfElse   <--- camelCase
+
+FizIfElse   <--- SnakeCase
+
+fiz_if_else  <---- fiz-if-else
+
+fizifelse  <---- wrong
+
+function fiz_if_else(number) {
+
+	if ( number % 3 === 0 && number % 5 === 0 ) {
+		console.log("fizzbuzz")
+	}
+	else if ( number % 3 === 0 ) {
+		console.log("fizz")
+	} 
+	else if ( number % 5 === 0 ) {
+		console.log("buzz")
+	} 
+	else {
+		console.log ("number not divisible by 3 or 5")
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+var  - variable - 
+
+ES6
+let - 
+const - constant - final - 
+
+
+
+function circumference(radius) {
+	const pi = 3.142
+
+	let circum = 2 * pi * radius
+
+	return circum
+}
+
+let k = circumference(4)
+
+console.log(k)
+
+function order(payment)
+{
+
+}
+console.log(p)
+
+
+
+// almighty formula
+
+//-b + Math.sqrt( Math.pow(b,2) - 4*a*c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
